2a20f512747fa7ff41b6cba7c95aaeff999fe783
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-24 20:41:55 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  board.each do |i|
+    if i == "X" || i == "O"
+      counter += 1
+    end
+    counter
+end
+
+def current_player(board)
+current_player = "X"
+  if !(turn_count(board) % 2 == 0)
+    current_player = "O"
+end
